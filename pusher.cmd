b526b477aa0d326cd2a0c793d15bb32fc6f6a69c
Andys100thedt <Andysotlk@outlook.com>
2026-03-07 07:34:30 +0800

    Cycle push tweak. qol

--- pusher.cmd
+++ pusher.cmd
@@ -2,4 +2,4 @@ D:
 cd "D:\meow\SHIT-preprints-metrics-history\"
 git add "./src"
 git commit -m %1
-git push
+for /l %%a in (0,0,1) do (echo %%a----------------------- && git push && goto :EOF; sleep 5)
