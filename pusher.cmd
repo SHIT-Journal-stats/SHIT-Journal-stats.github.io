a149af4a2cb05af1b5fa503d393e66c056b7958f
Andys100thedt <Andysotlk@outlook.com>
2026-03-07 09:00:34 +0800

    Fix no EOF, qol fix. tweak.

--- pusher.cmd
+++ pusher.cmd
@@ -2,4 +2,4 @@ D:
 cd "D:\meow\SHIT-preprints-metrics-history\"
 git add "./src"
 git commit -m %1
-for /l %%a in (0,0,1) do (echo %%a----------------------- && git push && goto :EOF; sleep 5)
+for /l %%a in (0,0,1) do (echo %%a----------------------- && git push && exit; sleep 5)
